a52321134d4bce16b89a703571f4df8ca7e254c6
Kevin Ratnasekera <djkevincr@yahoo.com>
2015-06-14 23:16:58 +0530

    serialization of metadata document

---
+++ lib/server-core/src/main/java/org/apache/olingo/server/core/serializer/json/MetadataDocumentJsonSerializer.java
@@ -0,0 +1,7 @@
+package org.apache.olingo.server.core.serializer.json;
+
+/**
+ * Created by djkevincr on 6/4/15.
+ */
+public class MetadataDocumentJsonSerializer {
+}
---
+++ lib/server-core/src/test/java/org/apache/olingo/server/core/serializer/json/MetadataDocumentJsonSerializerTest.java
@@ -0,0 +1,7 @@
+package org.apache.olingo.server.core.serializer.json;
+
+/**
+ * Created by djkevincr on 6/14/15.
+ */
+public class MetadataDocumentJsonSerializerTest {
+}
